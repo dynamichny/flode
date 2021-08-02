3c1693575dfccaff6923597f2ab1a7ed9bff871c
dynamichny <marcinpawlicki0@gmail.com>
2021-08-02 22:25:39 +0200

    Changed js files to ts, removed redundant screens

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,10 +1,6 @@
 export { default as CookbookScreen } from './CookbookScreen/CookbookScreen';
 export { default as CreateScreen } from './CreateScreen/CreateScreen';
 export { default as ExploreScreen } from './ExploreScreen/ExploreScreen';
-export { default as LoginScreen } from './LoginScreen/LoginScreen';
 export { default as PreviewScreen } from './PreviewScreen/PreviewScreen';
-export { default as ProfileScreen } from './ProfileScreen/ProfileScreen';
-export { default as RegisterScreen } from './RegisterScreen/RegisgerScreen';
-export { default as SharedScreen } from './SharedScreen/SharedScreen';
 export { default as IngredientsCompletnessScreen } from './IngredientsCompletnessScreen/IngredientsCompletnessScreen';
 export { default as CookingScreen } from './CookingScreen/CookingScreen';
